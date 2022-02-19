10afac7885856d1cbb3142eb7dc72bfd45f7ae44
saurinya <60077693+saurinya@users.noreply.github.com>
2022-02-19 10:09:47 -0500

    clean up

--- code/modules/data/dbs.ts
+++ code/modules/data/dbs.ts
@@ -1,7 +1,9 @@
+import { Person } from "./usermanagement";
+
 /**
  * The suite of card.
  */
-enum CardSuit
+export enum CardSuit
 {
     clubs,
     diamonds,
@@ -12,7 +14,7 @@ enum CardSuit
 /**
  * The card in a deck.
  */
-enum CardPip
+export enum CardPip
 {
     ace,
     two,
@@ -33,45 +35,15 @@ enum CardPip
 // Represents one card in a Deck.
 export class Card
 {
-     deck: Deck;
      suit: CardSuit;
      pip: CardPip;
 
-     public constructor (deck: Deck, suit: CardSuit, pip: CardPip)
+     public constructor (suit: CardSuit, pip: CardPip)
      {
-         this.deck = deck;
          this.suit = suit;
          this.pip = pip;
      }
 
-     getCardPoints (): number
-     {
-        switch (this.pip)
-        {
-            case CardPip.ace : return 10;
-            case CardPip.two : return 0;
-            case CardPip.three : {
-                if (this.suit == CardSuit.spades)
-                return 30;
-                else
-                return 0;
-            }
-            case CardPip.four : return 0;
-            case CardPip.five : return 5;
-            case CardPip.six : return 0;
-            case CardPip.seven : return 0;
-            case CardPip.eight : return 0;
-            case CardPip.nine : return 0;
-            case CardPip.ten : return 10;
-            case CardPip.jack : return 10;
-            case CardPip.queen : return 10;
-            case CardPip.king : return 10;
-            case CardPip.joker : return 0;
-            default : return 0;
-        }
-
-        return 0;
-     }
 
      printSuit () : string
      {
@@ -128,9 +100,11 @@ export class Deck
     public constructor (name: string)
     {
         this.name = name;
-        // Lets create the cards.
-        this.loadDeck();
     }   
+
+    getRandomInt(max: number) {
+        return Math.floor(Math.random() * max);
+    }        
 
     public getName (): string
     {
@@ -147,17 +121,6 @@ export class Deck
         this.cards.push(card);   
     }
 
-
-    getDeckPoints (): number
-    {
-        let numberofPoints = 0;
-        this.cards.forEach(card => {
-
-            numberofPoints += card.getCardPoints ();
-        })
-
-        return numberofPoints;
-    }
 
     removeCard (cardToRemove: Card)
     {
@@ -190,7 +153,7 @@ export class Deck
         let newCardArray = new Array<Card> ();
         while (this.cards.length > 0)
         {
-            let pick = getRandomInt(this.cards.length);
+            let pick = this.getRandomInt(this.cards.length);
             let card = this.cards.splice(pick,1);
             newCardArray.push(card.pop());
         }
@@ -200,65 +163,6 @@ export class Deck
         newCardArray.forEach(card=>{
             this.cards.push(card);
         })
-    }
-
-    loadDeck ()
-    {
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.ace));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.two));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.three));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.four));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.five));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.six));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.seven));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.eight));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.nine));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.ten));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.jack));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.queen));
-        this.addCardToDeck(new Card(this,CardSuit.clubs,CardPip.king));
-    
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.ace));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.two));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.three));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.four));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.five));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.six));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.seven));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.eight));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.nine));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.ten));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.jack));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.queen));
-        this.addCardToDeck(new Card(this,CardSuit.diamonds,CardPip.king));
-    
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.ace));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.two));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.three));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.four));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.five));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.six));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.seven));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.eight));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.nine));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.ten));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.jack));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.queen));
-        this.addCardToDeck(new Card(this,CardSuit.hearts,CardPip.king));
-    
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.ace));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.two));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.three));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.four));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.five));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.six));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.seven));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.eight));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.nine));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.ten));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.jack));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.queen));
-        this.addCardToDeck(new Card(this,CardSuit.spades,CardPip.king));        
     }
 
     printDeck ()
@@ -273,58 +177,23 @@ export class Deck
     }
 }
 
-export function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
-}    
 
 
-export class Person
-{
-    id: string;
-    name: string;
 
-    constructor (id: string,name: string)
-    {
-        this.id = id;
-        this.name = name;
-    }
-}
 
-export class Game 
+export abstract class BaseGame
 {
     uniqueId: string;   // The unique id of the game.
     createdAt: Date;    // The date when created;
     parentRoom: Room;   // The room to which the game belongs.
-    auctionAmount: number;
     players: Array<GamingPlayer> = new Array<GamingPlayer> (); // The players playing the game.
     observers: Array<GamingObserver> = new Array<GamingObserver> (); // Just folks who are watching..
-    leader: GamingPlayer;   // The person who is the leader (won the auction.)
-    trumpCardSuit: CardSuit;
     decks: Array<Deck> = new Array<Deck>();
-    completedHands: Array<Hand> = new Array<Hand>();
-    currentHand: Hand;
 
     constructor (id: string,room: Room)
     {
         this.uniqueId = id;
         this.parentRoom = room;
-    }
-
-    addPlayer (newPlayer: GamingPlayer): void
-    {
-        // Lets check to see that the player is not already present..
-        for (let player of this.players)
-        {
-            if (player.persona.id == newPlayer.persona.id)
-            {
-                return; // The player is already in the Game.
-            }
-        }
-
-        if (this.getNoOfPlayers () >= 10)
-            throw "Enough players already.."
-
-        this.players.push(newPlayer);
     }
 
     getNoOfPlayers (): number
@@ -337,85 +206,17 @@ export class Game
         return this.observers.length;
     }
 
-    /**
-     * Create the required number of Decks and remove extra cards as per no of folks;
-     */
-    createRequiredDecksAndClearUnRequiredCards ()
-    {
-        this.decks.length = 0;  // CLear the Decks..
-        let d1 = new Deck ("Deck1");
-        this.decks.push(d1);
-        let d2 = new Deck ("Deck2");
-
-        if (this.getNoOfPlayers () > 6)
-        this.decks.push(d2);
-        else
-        d2 = null;
-
-        let noOfCards = 52 * this.decks.length;
-        let cardsToRemove = noOfCards%this.getNoOfPlayers();
-
-        let cardsCanBeRemovedFromD1 = getRemovableCardsFromADeck ();
-        let cardsCanBeRemovedFromD2 = getRemovableCardsFromADeck ();
-
-        while (cardsToRemove > 0)
-        {
-            if (d1 != null)
-            {
-                let cardToRemove = cardsCanBeRemovedFromD1.pop();
-                if (cardToRemove == null)
-                    throw "No more removable cards in from Deck D1";
-                d1.removeCard(cardToRemove);
-                
-                cardsToRemove --;
-            }
-
-            if (d2 != null && cardsToRemove > 0)
-            {
-                let cardToRemove = cardsCanBeRemovedFromD2.pop();
-                if (cardToRemove == null)
-                    throw "No more removable cards in from Deck D2";
-
-                d2.removeCard(cardToRemove);
-                cardsToRemove --;
-            }
-        }
-    }
-
-
-    getNoOfPointsInGame (): number
-    {
-        let noOfPoints = 0;
-
-        for (let deck of this.decks)
-        {
-            noOfPoints += deck.getDeckPoints ();
-        }
-
-        return noOfPoints;
-    }
-
-
-    printGameInfo ()
-    {
-        console.log("GameId: " + this.uniqueId);
-        console.log("NoOfPlayers: " + this.getNoOfPlayers());
-        console.log("NoOfObservers: " + this.getNoOfObservers());
-        console.log("NoOfDecks: " + this.decks.length);
-        console.log("GamePoints: " + this.getNoOfPointsInGame ());
-
-        this.decks.forEach(element => {
-            console.log("- " + element.cards.length + "-");
-            element.printDeck ();
-            console.log("----------");
-        });
-    }
+    abstract getCardPoints (card: Card): number;
+    abstract addGamingPlayer (gamingPlayer: GamingPlayer);
 }
+
+
+
 
 export class GamingCard
 {   
     card: Card;     // The card thrown.
-    game: Game;     // The game that it was thrown in.
+    game: BaseGame;     // The game that it was thrown in.
     player: GamingPlayer;   // The player who owns the Card;
     hand: Hand; // The Hand that it belongs to.
 }
@@ -436,9 +237,9 @@ export class GamingPlayer
 {
     persona: Person;
     allotedCards: Array<Card> = new Array ();
-    game: Game; // The game that the player is in.
+    game: BaseGame; // The game that the player is in.
 
-    constructor(gm: Game,person: Person)
+    constructor(gm: BaseGame,person: Person)
     {
         this.persona=person;
         this.game = gm;    
@@ -470,45 +271,9 @@ export class Room
     uniqueId: string;
     roomName: string;
     admins: Array<Person> = new Array();  // Room Administrators.
-    games: Array<Game> = new Array<Game>();
-}
-
-export function getRemovableCardsFromADeck () : Array<Card>
-{
-    let removableCards = new Array ();
-    removableCards.push(new Card(this,CardSuit.clubs,CardPip.six));
-    removableCards.push(new Card(this,CardSuit.hearts,CardPip.six));
-    removableCards.push(new Card(this,CardSuit.spades,CardPip.six));
-    removableCards.push(new Card(this,CardSuit.diamonds,CardPip.six));
-    removableCards.push(new Card(this,CardSuit.clubs,CardPip.four));
-    removableCards.push(new Card(this,CardSuit.hearts,CardPip.four));
-    removableCards.push(new Card(this,CardSuit.spades,CardPip.four));
-    removableCards.push(new Card(this,CardSuit.diamonds,CardPip.four));
-    removableCards.push(new Card(this,CardSuit.clubs,CardPip.three));
-    removableCards.push(new Card(this,CardSuit.hearts,CardPip.three));
-    removableCards.push(new Card(this,CardSuit.diamonds,CardPip.three));
-    removableCards.push(new Card(this,CardSuit.clubs,CardPip.two));
-    removableCards.push(new Card(this,CardSuit.hearts,CardPip.two));
-    removableCards.push(new Card(this,CardSuit.spades,CardPip.two));
-    removableCards.push(new Card(this,CardSuit.diamonds,CardPip.two));
-    return removableCards;
+    games: Array<BaseGame> = new Array<BaseGame>();
 }
 
 
 
-let room = new Room();
-let game = new Game("XYZ",room);
-game.addPlayer(new GamingPlayer(game,new Person("xyz","Saurin")))
-game.addPlayer(new GamingPlayer(game,new Person("avd","Mita")))
-game.addPlayer(new GamingPlayer(game,new Person("xfd","Sonu")))
-game.addPlayer(new GamingPlayer(game,new Person("skd","Niyati")))
-game.addPlayer(new GamingPlayer(game,new Person("iek","Pramit")))
-// game.addPlayer(new GamingPlayer(game,new Person("sld","Urmil")))
-// game.addPlayer(new GamingPlayer(game,new Person("lfs","Dilip")))
-// game.addPlayer(new GamingPlayer(game,new Person("ekd","Priya")))
-// game.addPlayer(new GamingPlayer(game,new Person("3kf","Koyal")))
-game.addPlayer(new GamingPlayer(game,new Person("sow","Madhu")))
-
-game.createRequiredDecksAndClearUnRequiredCards ();
-game.printGameInfo ();
 
